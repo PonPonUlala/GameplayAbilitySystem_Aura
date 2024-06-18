bc81d8f17c8a1038207b8c09bd527e8178b2a5fd
Daniel Cooper <phkoopz@gmail.com>
2024-06-18 08:39:39 -0400

    Player Controller and Input Mapping

---
+++ Source/Aura/Private/Public/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// Copyright PonPon
+
+
+#include "Public/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Halt Execution if no AuraContext
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
